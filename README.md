845713489500c54478ddcf0de3ce21160ce4899b
David Nidorf <davini@outlook.com>
2015-04-26 15:55:08 -0700

    Adding run_analysis R script
    Adding the script to the repo, along with it's readme and codebook.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # GettingAndCleaningDataProject
 GettingAndCleaningDataProject
+
+The run_analysis.R script creates a tidy data set from the Samsung wearable study data which can be found at:
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+The script generates the tidy data set via the following steps:
+1. Extracts the raw data from the Samsung data files into a set of data frames,
+2. Merges the data frames together,
+3. Filters out all of the measurement data except for the mean and standard deviation of each measurement,
+4. Assigns descriptive names to each column,
+5. Creates a second, independent tidy data set with the average of the mean and standard deviations as grouped by activity and subject.
+
+More information about the data set can be found here:
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+
+License:
+========
+Note that use of the generated dataset in publications must be acknowledged by referencing the following publication [1] 
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+
+Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
---
+++ run_analysis.R
@@ -0,0 +1,58 @@
+# For more information on this function, please read the provided README file.
+run_Analysis <- function() {
+    # First, get the metadata mappings to help us name and understand the raw data
+    activityLabels<-read.table("UCIHARDataset/activity_labels.txt", col.names=c("ActivityLabel","Activity"))
+    featureLabels<-read.table("UCIHARDataset/features.txt", col.names=c("FeatureLabel","Feature"))
+    
+    # Note - we could do the below in a loop if there were more than two data sets, 
+    # but for the sake of simplicity this script just handles each data set explicitly for now.
+    
+    # Also note that the dplyr could be used here, but to reduce dependencies this solution
+    # only references the base package.
+    
+    # Get the Test data set
+    testData<-read.table("UCIHARDataset/test/X_test.txt")
+    testDataActivityLabels<-read.table("UCIHARDataset/test/y_test.txt")
+    names(testDataActivityLabels)<-c("ActivityLabel")
+    testDataSubjects<-read.table("UCIHARDataset/test/subject_test.txt")
+    names(testDataSubjects)<-c("Subject")
+    
+    # Get the Training data set
+    trainingData<-read.table("UCIHARDataset/train/X_train.txt")
+    trainingDataActivityLabels<-read.table("UCIHARDataset/train/y_train.txt")
+    names(trainingDataActivityLabels)<-c("ActivityLabel")
+    trainingDataSubjects<-read.table("UCIHARDataset/train/subject_train.txt")
+    names(trainingDataSubjects)<-c("Subject")
+    
+    # Apply descriptive measurement labels to the data columns
+    names(testData)<-featureLabels$Feature
+    names(trainingData)<-featureLabels$Feature
+    
+    # Filter down to the columns which contain mean or standard deviation information
+    # Based on the data descriptions this should be any measurement with the sequence "mean()" or "std()" in it.
+    meanAndStdColIndicies<-grep("mean[()]|std[()]", featureLabels[,2])
+    testData<-testData[,meanAndStdColIndicies]
+    trainingData<-trainingData[,meanAndStdColIndicies]
+    
+    # Merge the subject names to the data rows
+    labelledTestData<-cbind(testDataSubjects, testDataActivityLabels, testData)
+    labelledTrainingData<-cbind(trainingDataSubjects, trainingDataActivityLabels, trainingData)
+    
+    # Merge the two data sets together, by subject (subject values should be unique to each data set)
+    mergedData<-merge(labelledTestData, labelledTrainingData, all=TRUE)
+    
+    # Apply descriptive activity labels to the data rows
+    labelledData<-merge(mergedData, activityLabels, by.x="ActivityLabel", by.y="ActivityLabel", sort=FALSE)
+    
+    # Then drop the Activity raw value in favor of the descriptive name
+    tidyData<-labelledData[c(2,ncol(labelledData),4:ncol(labelledData)-1)]
+    
+    # Write out the full tidy data set
+    write.table(tidyData,"tidyData_full.txt",row.names=FALSE)
+    
+    # Create another tidy data set of the averages of each measurement grouped by Subject and Activity
+    aggregatedTidyData<-aggregate(tidyData[3:ncol(tidyData)], list(Subject=tidyData$Subject,Activity=tidyData$Activity), mean)
+    
+    # Write out the aggregated tidy data set
+    write.table(aggregatedTidyData,"tidyData.txt",row.names=FALSE)
+}
